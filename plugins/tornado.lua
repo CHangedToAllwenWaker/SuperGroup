5628aa549d8980edce4bfb294229f7652a2492c3
allwen <ArianGhasempour40@Gmail.com>
2016-06-02 17:31:44 +0430

    Update tornado.lua

--- plugins/tornado.lua
+++ plugins/tornado.lua
@@ -1,1 +1,15 @@
+do
 
+function run(msg, matches)
+   send_document(get_receiver(msg), "./data/sticker/animation.gif.mp4", ok_cb, false)
+end
+
+return {
+patterns = {
+"^![Tt]ornado$",
+
+},
+run = run
+}
+
+end
