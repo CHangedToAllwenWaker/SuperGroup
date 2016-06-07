cf6ae28d44d9dfcf8770e8a682f7bb736be04783
allwen <ArianGhasempour40@Gmail.com>
2016-06-07 17:56:21 +0430

    Update dad.lua

--- plugins/dad.lua
+++ plugins/dad.lua
@@ -1,8 +1,8 @@
 do
 
 function run(msg, matches)
-local reply_id = msg['105831687']
-local text = 'سلام بابایی جونم 😍'
+local reply_id = msg['174770816']
+local text = 'Hi My Dad ❤'
 if matches[1] == 'Slm' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
