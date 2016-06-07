3997e2b3f55a1fb356b6de6e4343eda934e3eee6
allwen <ArianGhasempour40@Gmail.com>
2016-06-07 17:49:01 +0430

    Say Focus !

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'tornado' then -- Put everything you like :)
+  if matches[1]:lower() == 'focus' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "tornado" then -- Put everything you like :)
+    if matches[2] == "focus" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (tornado)",
-	"^[#!/]([Tt]ornado)"
+    "^[#!/]([Ss]tats) (focus)",
+	"^[#!/]([Ff]ocus)"
     }, 
   run = run
 }
